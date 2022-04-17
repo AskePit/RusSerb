61be87def0ef681a78035b4113b1f088efa790ed
Nikolai Shalakin <nikolai.shalakin@lifeisfeudal.com>
2022-04-17 19:19:03 +0200

    separate load code

---
+++ code/load.py
@@ -0,0 +1,157 @@
+import io
+import sys
+#import re
+from code.core_types import *
+import glob
+import os
+
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
+
+SEPARATORS_REGEXP: str = '\n'
+
+def ConvertLinesToTokens(lines: list[str]) -> list[str]:
+    megaLine = ''.join(lines)
+    #res = re.split(SEPARATORS_REGEXP, megaLine)
+    res = megaLine.split(SEPARATORS_REGEXP)
+    res[:] = [x for x in res if x]
+    return res
+    
+class Header:
+    speechPart = SpeechPart.noun
+    formsSequence: list[Declination] = []
+    cursor = 0
+            
+    def parseHeader(self, speechPart: SpeechPart, lines: list[str]):
+        self.speechPart = speechPart
+        
+        parseSequence = ConvertLinesToTokens(lines)
+        for seq in parseSequence:
+            self.formsSequence.append(Declination.Make(speechPart, seq))
+        
+    def yieldDeclination(self) -> Declination:
+        if len(self.formsSequence) == 0:
+            return None
+
+        if self.cursor >= len(self.formsSequence):
+            return None
+        
+        res = self.formsSequence[self.cursor]
+        self.cursor = self.cursor + 1
+        return res
+    
+    def resetYield(self):
+        self.cursor = 0
+
+def loadHeadered(data: list[str], speechPart: SpeechPart, theWords: WordList):        
+    headerLines = 0
+    headerRed = False
+    
+    header = Header()
+    
+    #header read
+    for l in data:
+        if l.startswith('-'):
+            headerRed = True
+            continue
+        
+        if not headerRed:
+            headerLines = headerLines + 1
+            continue
+    
+    header.parseHeader(speechPart, data[:headerLines])
+    
+    # read data
+    data = data[headerLines+1:]
+    data = ConvertLinesToTokens(data)
+
+    firstTitle = ""
+    if len(data):
+        firstTitle = data[0].split('/')[0]
+    
+    theWord = Word.MakeTitled(speechPart, firstTitle)
+
+    for wordPair in data:
+        words = wordPair.split('/')
+        rus = words[0]
+        serb = words[1]
+
+        form = header.yieldDeclination()
+        if form == None:
+            theWords.words.append(theWord)
+
+            # new theWord
+            theWord = Word.MakeTitled(speechPart, rus)
+
+            header.resetYield()
+            form = header.yieldDeclination()
+        
+        theWord.forms.append(DeclinedWord.Make(form, rus, serb))
+        
+    theWords.words.append(theWord)
+
+def loadFixed(data: list[str], theWord: Word):
+    # read data
+    data = ConvertLinesToTokens(data)
+
+    title = ""
+    if len(data):
+        p = data[0].split('/')
+        title = p[0] if len(p[0]) else p[1]
+    
+    theWord.title = title
+    
+    declinationTime = False
+    rus = ""
+    serb = ""
+
+    for w in data:
+        if declinationTime:
+            declinationTime = False
+            declinedWord = DeclinedWord.Make(Declination.Make(SpeechPart.tobe, w), rus, serb)
+            theWord.forms.append(declinedWord)
+        else:
+            declinationTime = True
+
+            words = w.split('/')
+            rus = words[0]
+            serb = words[1]
+            continue
+    
+    theWord.normalize()
+
+def loadFile(filename: str):
+    with io.open(filename, encoding='utf-8') as f:
+        data = f.readlines()
+        if not len(data):
+            return
+        
+        headerP = data[0].strip().split(' ')
+        headerType = headerP[0]
+        speechPart = SpeechPart[headerP[1]]
+
+        data = data[2:]
+
+        if headerType == 'header':
+            theWords = WordList()
+            loadHeadered(data, speechPart, theWords)
+            return theWords
+        elif headerType == 'fixed':
+            theWord = Word.Make(speechPart)
+            loadFixed(data, theWord)
+            return theWord
+
+vocabulary = {}
+
+for f in glob.glob('**/*.txt', recursive=True):
+    collectionName = os.path.splitext(os.path.basename(f))[0]
+    data = loadFile(f)
+    vocabulary[collectionName] = data
+
+# shortcuts
+occupations       = vocabulary['occupations']
+personal_pronouns = vocabulary['personal_pronouns']
+tobe              = vocabulary['tobe']
+positive_tobe     = vocabulary['positive_tobe']
+negative_tobe     = vocabulary['negative_tobe']
+question_tobe     = vocabulary['question_tobe']
