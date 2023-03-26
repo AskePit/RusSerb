b8405a6b3c318455c65a5a74ee1ff29e3e231058
Nikolai Shalakin <nikolai.shalakin@lifeisfeudal.com>
2023-03-26 21:20:11 +0200

    serb_gui initial commit

---
+++ gui/script.js
@@ -0,0 +1,46 @@
+window.addEventListener('pywebviewready', function() {
+    var container = document.getElementById('catalogue')
+    container.innerHTML = '<i>pywebview</i> is ready'
+
+    showCurrentDirContent()
+})
+
+// Create event listener
+document.addEventListener('click', (e) =>
+  {
+    // Retrieve id from clicked element
+    let elementId = e.target.id;
+    // If element has id
+    if (elementId !== '') {
+        pywebview.api.onExcClicked(parseInt(elementId, 10))
+    }
+    // If element has no id
+    else { 
+        //console.log("An element without an id was clicked.");
+    }
+  }
+);
+
+function showCurrentDirContent() {
+    pywebview.api.getCurrentDirContent().then(onCurrentDirContentReady)
+}
+
+function onCurrentDirContentReady(content) {
+    //console.log(content)
+
+    var catalogue = document.getElementById('catalogue')
+
+    html = ''
+
+    for(let i = 0; i<content.length; ++i) {
+        let exc = content[i]
+        //console.log(exc)
+        html += `
+            <div class="excercise_item" id="${i}">
+            ${exc.isDir ? "〉" : ""}${exc.name}
+            </div>
+        `
+    }
+
+    catalogue.innerHTML = html
+}
---
+++ serb_gui.py
@@ -0,0 +1,60 @@
+from code.load import *
+from code.excercise_types import *
+from code.cutom_excercises import *
+
+import webview
+
+class Api:
+    def __init__(self, excercises) -> None:
+        self.currentDir = excercises
+        self.numToEx = {}
+    
+    def setWindow(self, window):
+        self.window = window
+
+    def getCurrentDirContent(self):
+        files = []
+
+        for d in self.currentDir.children:
+            files.append(d)
+
+        for e in self.currentDir.excercises:
+            files.append(e)
+        
+        files.sort(key=lambda x: x.serialNumber)
+
+        i = 0
+        self.numToEx = {}
+
+        response = []
+
+        for f in files:
+            response.append({"isDir": isinstance(f, ExcerciseDescsDir), "name": f.name})
+
+            self.numToEx[i] = f
+            i += 1
+
+        return response
+    
+    def onExcClicked(self, excIndex):
+        if excIndex in self.numToEx:
+            exc = self.numToEx[excIndex]
+            if isinstance(exc, ExcerciseDescsDir):
+                self.currentDir = exc
+                self.window.evaluate_js("showCurrentDirContent()")
+            elif isinstance(exc, ExcerciseDesc):
+                #ExecuteExcercises([exc], screen, input)
+                print(f"execute excercise {exc.name}")
+
+def main():
+    LoadVocabulary('./vocabulary')
+    LoadManuals('./mans')
+    excercises = LoadExcercises()
+
+    api = Api(excercises)
+    window = webview.create_window('Serb', 'gui/index.html', js_api=api)
+    api.setWindow(window)
+    webview.start(debug=True)
+
+if __name__ == '__main__':
+    main()
